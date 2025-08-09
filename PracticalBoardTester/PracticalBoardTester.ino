346c91ebb9415b6897e80ddc8b58b695ed3a740f
Lilan Manoj <lilanmanoj@gmail.com>
2025-08-09 17:15:08 +0530

    Syncing SSD/LED patterns done

--- PracticalBoardTester/PracticalBoardTester.ino
+++ PracticalBoardTester/PracticalBoardTester.ino
@@ -1,20 +1,27 @@
 #include<Arduino.h>
 
-/* Constants */
+/* Program Specific Constants */
 const unsigned int MAX_CYCLE_COUNT = 1000;
 const unsigned int DATA_PINS[8] = {2, 3, 4, 5, 6, 7, 14, 15};
 const unsigned int SWITCHING_PINS[4] = {16, 17, 18, 19};
 const unsigned int LED_SSD_PINS[2] = {12, 13};
 const unsigned int LED_GND_INDEX = 0;
 const unsigned int SSD_GND_INDEX = 1;
-const unsigned int PROGRAM_DELAY = 5;
-const unsigned int LED_CYCLES_MULTIPLIER = 10;
-unsigned int global_cycle_count = 0;
-unsigned int led_cycle_count = 0;
-bool led_is_counting_up = true;
-unsigned int led_current_index = 0;
+const unsigned int PROGRAM_DELAY = 2;
+const unsigned int LED_CYCLE_RATIO = 15;
+const unsigned int SSD_CYCLE_RATIO = 100;
 
-/* SSD */
+/* Variables */
+unsigned int global_cycle_count = 0;
+bool led_is_counting_up = true;
+unsigned int led_cycle_count = 0;
+unsigned int ssd_cycle_count = 0;
+unsigned int led_active_index = 0;
+unsigned int ssd_active_index = 0;
+unsigned int ssdx = 0;
+
+
+/* SSD Constants */
 const unsigned int DIGIT_MAP[10] = {
     0x3f,
     0x06,
@@ -52,24 +59,43 @@ void setup() {
 void loop() {
     global_cycle_count++;
 
-    if (global_cycle_count%LED_CYCLES_MULTIPLIER == 0) {
+    /* LED Test */
+    if (global_cycle_count%LED_CYCLE_RATIO == 0) {
         led_cycle_count = (led_cycle_count < 7) ? led_cycle_count + 1 : 0;
 
         if (led_is_counting_up) {
-            led_current_index = led_cycle_count;
+            led_active_index = led_cycle_count;
             led_is_counting_up = (led_cycle_count >= 7) ? false : true;
         } else {
-            led_current_index = 7 - led_cycle_count;
+            led_active_index = 7 - led_cycle_count;
             led_is_counting_up = (led_cycle_count >= 7) ? true : false;
         }        
-    } 
+    }
     
-    LEDWrite(led_current_index);
+    LEDWrite(led_active_index);
 
     delay(PROGRAM_DELAY);
 
-    SSDSelect(0);
-    SSDWrite(9);
+    /* SSD Test */
+    if (global_cycle_count%SSD_CYCLE_RATIO == 0) {
+        if (ssd_active_index < 4) {
+            ssd_active_index = (ssd_cycle_count == 8) ? ssd_active_index + 1 : ssd_active_index;
+            ssd_cycle_count = (ssd_cycle_count <= 7) ? ssd_cycle_count + 1 : 0;
+        } else {
+            ssd_active_index = 0;
+            ssd_cycle_count = 0;
+        }
+    }
+    
+    SSDSelect(ssdx);
+
+    if (ssdx == ssd_active_index) {
+        SSDWrite(ssd_cycle_count);
+        ssdx=0;
+    } else {
+        SSDWrite(8);
+        ssdx++;
+    }
 
     global_cycle_count = (global_cycle_count == MAX_CYCLE_COUNT) ? 0 : global_cycle_count;
 
