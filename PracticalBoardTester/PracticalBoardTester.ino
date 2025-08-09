599e8321e0fcbadb5abeed4f2a896ca14f81acc2
Lilan Manoj <lilanmanoj@gmail.com>
2025-08-09 17:37:24 +0530

    Added readme notes and other supporting files related to practical board

--- PracticalBoardTester/PracticalBoardTester.ino
+++ PracticalBoardTester/PracticalBoardTester.ino
@@ -1,3 +1,13 @@
+/*
+* This is a simple program to test the practical board
+* 
+* Connetion help:
+*  - Practical board pins counting from left to right
+*  - Practical board data pins [0 - 5] => Arduino pins [2 - 7]
+*  - Practical board data pins [6 - 7] => Arduino pins [14 - 15]
+*  - Practical board control pins [0 - 3] => Arduino pins [19 - 16]
+*  - Practical board control pins [4 - 5] => Arduino pins [12 - 13]
+*/
 #include<Arduino.h>
 
 /* Program Specific Constants */
