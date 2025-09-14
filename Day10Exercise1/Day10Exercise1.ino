9752abe8378d08725614a2973fc1506ae36f94cb
Lilan Manoj <lilanmanoj@gmail.com>
2025-09-14 11:33:55 +0530

    Fixed bug

--- Day10Exercise1/Day10Exercise1.ino
+++ Day10Exercise1/Day10Exercise1.ino
@@ -1,8 +1,9 @@
 #include <Arduino.h>
 
-const uint8_t LED1 = 2;
-const uint8_t LED2 = 3;
+const int LED1 = 2;
+const int LED2 = 3;
 
+unsigned long currentMil = 0;
 unsigned long led1LastMil = 0;
 unsigned long led2LastMil = 0;
 
@@ -22,27 +23,29 @@ void setup() {
 }
 
 void loop() {
-    if (millis() - led1LastMil >= 300) {
+    currentMil = millis();
+
+    if (currentMil - led1LastMil >= 300) {
         // Serial com for debug purposes
         // Serial.print(millis() - led1LastMil);
         // Serial.print(" | ");
         Serial.print(led1State);
         Serial.println(" | Led 1");
 
-        led1LastMil = millis();
+        led1LastMil = currentMil;
         led1State = !led1State;
 
         digitalWrite(LED1, led1State);
     }
 
-    if (millis() - led2LastMil >= 1000) {
+    if (currentMil - led2LastMil >= 1000) {
         // Serial com for debug purposes
         // Serial.print(millis() - led2LastMil);
         // Serial.print(" | ");
         Serial.print(led2State);
         Serial.println(" | Led 2");
 
-        led2LastMil = millis();
+        led2LastMil = currentMil;
         led2State = !led2State;
 
         digitalWrite(LED2, led2State);
